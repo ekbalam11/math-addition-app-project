5d1b1888dd8aaa688b0e1509e574830c3ce643a4
ekbalam11 <ekbalam11@gmail.com>
2024-05-07 18:36:38 +0200

    random numbers creation and sum

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // Buena suerte!
+//primary-number
+
+//secondary-number
+
+function getRandomInt() {
+    return Math.floor(Math.random()*10);
+}
+
+var primaryNumber = getRandomInt();
+var secondaryNumber = getRandomInt();
+
+console.log("This is the random number 1: ", primaryNumber);
+console.log("This is the random number 2: ", secondaryNumber);
+
+var numSum = primaryNumber+secondaryNumber;
+
+console.log("This is the sum of both random numbers: ", numSum);
+
+const resultcheck = document.querySelector('#btn');
+resultcheck.addEventListener('click', function(e)){
+    e.target.classList.t
+}
